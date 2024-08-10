afb3f99026f545d12e06c3407f64628867514859
Mayara <mayara.lira092@gmail.com>
2024-08-10 16:45:36 -0300

    feat: Tornando página responsiva e corrigindo problemas com imagens.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 document.getElementById('nome').value = 'Pesquisa';
+
+function myFunction() {
+    LET x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
